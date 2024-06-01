f03f7bc74e1715bcd80d6352ae5fbb5e31dce0ae
hienvm <hien2842003@gmail.com>
2024-06-01 21:10:24 +0700

    cmp

--- demo.py
+++ demo.py
@@ -16,22 +16,14 @@ def main():
         [' ', ' ', ' ', ' ', ' ', ' ', ' '],
         [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ]
-    # board = np.array([[toCell(s) for s in row]
-    #                   for row in board], dtype=np.uint8)
-    # h = Heuristic(5, 1)
-    # h.build(board)
-    # board[2][1] = 1
-    # h.update(2, 1)
-    # board[4][4] = 2
-    # h.update(4, 4)
 
-    # print(board)
-    # print(h.sum())
+    board = [[' ' for i in range(15)] for j in range(15)]
+    board[0][0] = 'o'
 
     ai = TicTacToeAi(5, 'x', max_depth=2)
 
     begin = perf_counter_ns()
-    for i in range(2):
+    for i in range(1):
         move = ai.get_move(board)
     end = perf_counter_ns()
     print(f"Time: {end - begin}")
@@ -40,6 +32,10 @@ def main():
         board[move[0]][move[1]] = 'x'
     for row in board:
         print('|' + '|'.join([f'{cell}' for cell in row]) + '|')
+
+    print(100732387800 / 17009103400)
+    print(16415071000)
+
     # begin = perf_counter_ns()
     # for i in range(10000):
     #     r = h.sum()
@@ -51,6 +47,16 @@ def main():
     # print(f'Pruned nodes: {ai.prune}')
     # print(f'Pruning rate: {ai.get_prune_rate()}')
 
+    # board = np.array([[toCell(s) for s in row]
+    #                   for row in board], dtype=np.uint8)
+    # h = Heuristic(5, 1)
+    # h.build(board)
+    # board[2][1] = 1
+    # h.update(2, 1)
+    # board[4][4] = 2
+    # h.update(4, 4)
+    # print(board)
+    # print(h.sum())
     # h = Heuristic(5, 1)
     # h.build(board)
     # begin = perf_counter_ns()
